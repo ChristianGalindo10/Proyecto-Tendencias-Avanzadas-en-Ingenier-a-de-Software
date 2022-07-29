b8f6b68065226a4129bd7e5640d0060fec7e5810
ChristianGalindo10 <cygc1@hotmail.com>
2022-07-28 19:58:40 -0500

    D3js example

--- static/js/main.js
+++ static/js/main.js
@@ -11,6 +11,59 @@ $("#queryForm").submit(function (e) {
     }
     $(".loader-container").css("display", "block");
     $("#results").html("");
+    var margin = { top: 20, right: 20, bottom: 30, left: 40 },
+        width = 960 - margin.left - margin.right,
+        height = 500 - margin.top - margin.bottom;
+
+    // set the ranges
+    var x = d3.scaleBand()
+        .range([0, width])
+        .padding(0.1);
+    var y = d3.scaleLinear()
+        .range([height, 0]);
+
+    // append the svg object to the body of the page
+    // append a 'group' element to 'svg'
+    // moves the 'group' element to the top left margin
+    var svg = d3.select("body").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
+
+    // get the data
+    d3.csv("/static/csv/XYZ.csv").then(function (data) {
+
+        // format the data
+        data.forEach(function (d) {
+            d.sales = +d.sales;
+        });
+
+        // Scale the range of the data in the domains
+        x.domain(data.map(function (d) { return d.salesperson; }));
+        y.domain([0, d3.max(data, function (d) { return d.sales; })]);
+
+        // append the rectangles for the bar chart
+        svg.selectAll(".bar")
+            .data(data)
+            .enter().append("rect")
+            .attr("class", "bar")
+            .attr("x", function (d) { return x(d.salesperson); })
+            .attr("width", x.bandwidth())
+            .attr("y", function (d) { return y(d.sales); })
+            .attr("height", function (d) { return height - y(d.sales); });
+
+        // add the x Axis
+        svg.append("g")
+            .attr("transform", "translate(0," + height + ")")
+            .call(d3.axisBottom(x));
+
+        // add the y Axis
+        svg.append("g")
+            .call(d3.axisLeft(y));
+
+    });
     $.ajax({
         url: u,
         type: "POST",
@@ -37,7 +90,6 @@ function results(results) {
     $("#results").load("/results_template", function () {
         $("#result_heading").html('<hr/><h1 align="center">Results</h1>');
         $.each(results, function (key, value) {
-            console.log(results[key]["link_product"].length);
             for (var i = 0; i < results[key]["link_product"].length; i++) {
                 var trs = '<tr>' +
                     '<td class="a">' +
@@ -51,7 +103,6 @@ function results(results) {
                     '</td>' +
                     "</tr>";
                 $("#cuerpoResultados").append(trs);
-                console.log(trs);
             }
         });
     });
